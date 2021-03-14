a6d824c9f9347d9a115e825a6054a7d3c7fc23d4
memonsahil <sahilm7@icloud.com>
2021-03-14 00:32:02 +0000

    Update context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,29 +1,16 @@
 import createDataContext from "./createDataContext";
 import jsonServer from "../api/jsonServer";
 
-//The initial state is created in the Provider within createDataContext().
-//The state for blogContext is passed as an array to createDataContext() below.
 const blogReducer = (state, action) => {
   switch (action.type) {
     case "get_blogposts":
       return action.payload;
-    /*
-        case 'add_blogPost':
-          return [
-              ...state,
-              {id: Math.floor(Math.random() * 99999),
-                title: action.payload.title,
-                content: action.payload.content}
-            ];
-        */
     case "delete_blogPost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
-    //state.filter returns the blogPosts whose id is not equal to action.payload (id to be deleted).
     case "edit_blogPost":
       return state.map((blogPost) => {
         return blogPost.id === action.payload.id ? action.payload : blogPost;
       });
-    //Return the mapped (updated) state on that particular blogPost based on the return value from the ternary condition.
     default:
       return state;
   }
@@ -33,20 +20,18 @@ const blogReducer = (state, action) => {
 
 const getBlogPosts = (dispatch) => {
   return async () => {
-    const response = await jsonServer.get("/blogposts"); //Getting blogposts from the jsonServer.
+    const response = await jsonServer.get("/blogposts");
 
     dispatch({ type: "get_blogposts", payload: response.data });
   };
 };
 
-const addBlogPosts = (dispatch) => {
+const addBlogPosts = () => {
   return async (title, content, callback) => {
+    //dispatch() is not invoked since the title and content are passed through jsonServer.post().
     await jsonServer.post("/blogposts", { title, content });
 
-    //dispatch() is not invoked since title and content are passed through jasonServer.post().
-    //dispatch({type: 'add_blogPost', payload: {title, content}}); //Same as title: title, content: content
     if (callback) {
-      //If a callback() function exits, then callback().
       callback();
     }
   };
@@ -69,7 +54,6 @@ const editBlogPosts = (dispatch) => {
       payload: { id: id, title: title, content: content },
     });
     if (callback) {
-      //If a callback() function exits, then callback().
       callback();
     }
   };
--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -1,23 +1,19 @@
-//Context helps pass values to any nested component within the { children } object.
-//File name starts with lowercase since it's exporting a plain function.
-
 import React, { useReducer } from "react";
 
 export default (reducer, actions, initialState) => {
-  const Context = React.createContext(); //React has a function for implementing context.
+  //Context helps pass values to any nested component within the { children } object.
+  const Context = React.createContext();
 
   const Provider = ({ children }) => {
     const [state, disptach] = useReducer(reducer, initialState);
 
-    //actions == { addBlogPosts: (dispatch) => { return () => {} } }
     const boundActions = {}; //Empty object to store actions that are bound to dispatch.
     for (let key in actions) {
-      boundActions[key] = actions[key](disptach); //Passes dispatch to the functions within actions and stores them within boundActions.
+      //Passes dispatch to the functions within actions and stores them within boundActions.
+      boundActions[key] = actions[key](disptach);
     }
 
     return (
-      //state same as state: state in the object below.
-      //Passing the entire boundActions object below.
       <Context.Provider value={{ state, ...boundActions }}>
         {children}
       </Context.Provider>
