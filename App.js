21cdf0444e4132ca7be0dcb9b7bd2cbdc6585a6a
memonsahil <sahilm7@icloud.com>
2021-03-14 00:15:14 +0000

    Update comments

--- App.js
+++ App.js
@@ -2,8 +2,7 @@ import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import IndexScreen from "./src/screens/IndexScreen";
-import { Provider } from "./src/context/BlogContext"; //Using curley braces to import Provider since it is not a default export from that file.
-//import { Provider as CommentsProvider } from './src/context/CommentsContext'; Renaming the Provider import based on its data type if multiple Providers are used.
+import { Provider } from "./src/context/BlogContext";
 import ShowScreen from "./src/screens/ShowScreen";
 import CreateScreen from "./src/screens/CreateScreen";
 import EditScreen from "./src/screens/EditScreen";
@@ -27,19 +26,8 @@ const App = createAppContainer(navigator);
 
 export default () => {
   return (
-    //Provider wraps the App (therfore, all components within).
     <Provider>
       <App />
     </Provider>
   );
-
-  /*
-   return ( //When multiple Providers are used, their wrapping hierarchy does not matter.
-    <CommentsProvider>
-      <Provider>
-        <App />
-      </Provider>
-    </CommentsProvider>
-   );
-  */
 };
