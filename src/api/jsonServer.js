ffe675a0aaec7cc51c6910c116cb42992cc0f59f
Sahil Memon <sahilm1899@gmail.com>
2021-06-15 18:44:28 +0100

    Update jsonServer.js

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 /*
-Launch a local json database from the blog-post-server project. ngrok is setup in
-the blog-post-server project and is used to redirect the baseURL to the database.
-Replace the ngrok forwareded URL with the current baseURL. Note that the ngrok 
-forwarded URL changes every 8 hours.
+Launch a local JSON database from the blog-post-server project. ngrok is setup in
+blog-post-server and is used to redirect the baseURL to the database. Replace the
+current baseURL with the ngrok forwareded URL. Note that the ngrok forwarded URL
+changes every 2 hours; therefore, it needs to be updated accordingly.
 */
 
 export default axios.create({
-  baseURL: "http://afc1c7c69ba5.ngrok.io",
+  baseURL: "https://5e9f78e37d0f.ngrok.io",
 });
