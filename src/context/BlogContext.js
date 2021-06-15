19c3bcc59180a0a1fd2f682b53f1a8d1160be3d6
Sahil Memon <sahilm1899@gmail.com>
2021-06-15 16:47:44 +0100

    Update context API

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,6 +1,43 @@
 import createDataContext from "./createDataContext";
 import jsonServer from "../api/jsonServer";
 
+const getBlogPosts = (dispatch) => {
+  return async () => {
+    const response = await jsonServer.get("/blogposts");
+    dispatch({ type: "get_blogposts", payload: response.data });
+  };
+};
+
+const addBlogPosts = () => {
+  // dispatch() is not invoked since the title and content are passed through jsonServer.post().
+  return async (title, content, callback) => {
+    await jsonServer.post("/blogposts", { title, content });
+    if (callback) {
+      callback();
+    }
+  };
+};
+
+const deleteBlogPosts = (dispatch) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`);
+    dispatch({ type: "delete_blogPost", payload: id });
+  };
+};
+
+const editBlogPosts = (dispatch) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
+    dispatch({
+      type: "edit_blogPost",
+      payload: { id: id, title: title, content: content },
+    });
+    if (callback) {
+      callback();
+    }
+  };
+};
+
 const blogReducer = (state, action) => {
   switch (action.type) {
     case "get_blogposts":
@@ -14,49 +51,6 @@ const blogReducer = (state, action) => {
     default:
       return state;
   }
-};
-
-//Action functions:
-
-const getBlogPosts = (dispatch) => {
-  return async () => {
-    const response = await jsonServer.get("/blogposts");
-
-    dispatch({ type: "get_blogposts", payload: response.data });
-  };
-};
-
-const addBlogPosts = () => {
-  return async (title, content, callback) => {
-    //dispatch() is not invoked since the title and content are passed through jsonServer.post().
-    await jsonServer.post("/blogposts", { title, content });
-
-    if (callback) {
-      callback();
-    }
-  };
-};
-
-const deleteBlogPosts = (dispatch) => {
-  return async (id) => {
-    await jsonServer.delete(`/blogposts/${id}`);
-
-    dispatch({ type: "delete_blogPost", payload: id });
-  };
-};
-
-const editBlogPosts = (dispatch) => {
-  return async (id, title, content, callback) => {
-    await jsonServer.put(`/blogposts/${id}`, { title, content });
-
-    dispatch({
-      type: "edit_blogPost",
-      payload: { id: id, title: title, content: content },
-    });
-    if (callback) {
-      callback();
-    }
-  };
 };
 
 export const { Context, Provider } = createDataContext(
--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -1,15 +1,19 @@
 import React, { useReducer } from "react";
 
+/*
+Context API:
+Context helps pass values to any nested component within the { children } object.
+*/
+
 export default (reducer, actions, initialState) => {
-  //Context helps pass values to any nested component within the { children } object.
   const Context = React.createContext();
 
   const Provider = ({ children }) => {
     const [state, disptach] = useReducer(reducer, initialState);
 
-    const boundActions = {}; //Empty object to store actions that are bound to dispatch.
+    // Empty object to store actions that are bound to dispatch.
+    const boundActions = {};
     for (let key in actions) {
-      //Passes dispatch to the functions within actions and stores them within boundActions.
       boundActions[key] = actions[key](disptach);
     }
 
